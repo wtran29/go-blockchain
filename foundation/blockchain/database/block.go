85ea3098fb8ef45028b2e98679882ece4c150221
Bi11y G04t <wtran292gmail.com>
2022-12-21 15:37:31 -0800

    included reset function to reset the state of the blockchain node, clean up on account and block files

--- foundation/blockchain/database/block.go
+++ foundation/blockchain/database/block.go
@@ -3,6 +3,8 @@ package database
 import (
 	"context"
 	"crypto/rand"
+	"errors"
+	"fmt"
 	"math"
 	"math/big"
 	"time"
@@ -10,6 +12,10 @@ import (
 	"github.com/wtran29/go-blockchain/foundation/blockchain/merkle"
 	"github.com/wtran29/go-blockchain/foundation/blockchain/signature"
 )
+
+// ErrChainForked is returned from validateNextBlock if another node's chain
+// is two or more blocks ahead of ours.
+var ErrChainForked = errors.New("blockchain forked, start resync")
 
 // =============================================================================
 
@@ -192,6 +198,75 @@ func (b Block) Hash() string {
 	//   blocks, but can prove a transaction is in a block.
 
 	return signature.Hash(b.Header)
+}
+
+// ValidateBlock takes a block and validates it to be included into the blockchain.
+func (b Block) ValidateBlock(previousBlock Block, stateRoot string, evHandler func(v string, args ...any)) error {
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: chain is not forked", b.Header.Number)
+
+	// The node who sent this block has a chain that is two or more blocks ahead
+	// of ours. This means there has been a fork and we are on the wrong side.
+	nextNumber := previousBlock.Header.Number + 1
+	if b.Header.Number >= (nextNumber + 2) {
+		return ErrChainForked
+	}
+
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: block difficulty is the same or greater than parent block difficulty", b.Header.Number)
+
+	if b.Header.Difficulty < previousBlock.Header.Difficulty {
+		return fmt.Errorf("block difficulty is less than previous block difficulty, parent %d, block %d", previousBlock.Header.Difficulty, b.Header.Difficulty)
+	}
+
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: block hash has been solved", b.Header.Number)
+
+	hash := b.Hash()
+	if !isHashSolved(b.Header.Difficulty, hash) {
+		return fmt.Errorf("%s invalid block hash", hash)
+	}
+
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: block number is the next number", b.Header.Number)
+
+	if b.Header.Number != nextNumber {
+		return fmt.Errorf("this block is not the next number, got %d, exp %d", b.Header.Number, nextNumber)
+	}
+
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: parent hash does match parent block", b.Header.Number)
+
+	if b.Header.PrevBlockHash != previousBlock.Hash() {
+		return fmt.Errorf("parent block hash doesn't match our known parent, got %s, exp %s", b.Header.PrevBlockHash, previousBlock.Hash())
+	}
+
+	if previousBlock.Header.TimeStamp > 0 {
+		evHandler("database: ValidateBlock: validate: blk[%d]: check: block's timestamp is greater than parent block's timestamp", b.Header.Number)
+
+		parentTime := time.Unix(int64(previousBlock.Header.TimeStamp), 0)
+		blockTime := time.Unix(int64(b.Header.TimeStamp), 0)
+		if blockTime.Before(parentTime) {
+			return fmt.Errorf("block timestamp is before parent block, parent %s, block %s", parentTime, blockTime)
+		}
+
+		// This is a check that Ethereum does but we can't because we don't run all the time.
+
+		// evHandler("database: ValidateBlock: validate: blk[%d]: check: block is less than 15 minutes apart from parent block", b.Header.Number)
+
+		// dur := blockTime.Sub(parentTime)
+		// if dur.Seconds() > time.Duration(15*time.Second).Seconds() {
+		// 	return fmt.Errorf("block is older than 15 minutes, duration %v", dur)
+		// }
+	}
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: state root hash does match current database", b.Header.Number)
+
+	if b.Header.StateRoot != stateRoot {
+		return fmt.Errorf("state of the accounts are wrong, current %s, expected %s", stateRoot, b.Header.StateRoot)
+	}
+
+	evHandler("database: ValidateBlock: validate: blk[%d]: check: merkle root does match transactions", b.Header.Number)
+
+	if b.Header.TransRoot != b.MerkleTree.RootHex() {
+		return fmt.Errorf("merkle root does not match transactions, got %s, exp %s", b.MerkleTree.RootHex(), b.Header.TransRoot)
+	}
+
+	return nil
 }
 
 // isHashSolved checks the hash to make sure it complies with
--- foundation/blockchain/database/database.go
+++ foundation/blockchain/database/database.go
@@ -3,9 +3,13 @@
 package database
 
 import (
+	"errors"
+	"fmt"
+	"sort"
 	"sync"
 
 	"github.com/wtran29/go-blockchain/foundation/blockchain/genesis"
+	"github.com/wtran29/go-blockchain/foundation/blockchain/signature"
 )
 
 // Storage interface represents the behavior required to be implemented by any
@@ -54,27 +58,27 @@ func New(genesis genesis.Genesis, storage Storage, evHandler func(v string, args
 		db.accounts[accountID] = newAccount(accountID, balance)
 	}
 
-	// // Read all the blocks from storage.
-	// iter := db.ForEach()
-	// for block, err := iter.Next(); !iter.Done(); block, err = iter.Next() {
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
+	// Read all the blocks from storage.
+	iter := db.ForEach()
+	for block, err := iter.Next(); !iter.Done(); block, err = iter.Next() {
+		if err != nil {
+			return nil, err
+		}
 
-	// 	// Validate the block values and cryptographic audit trail.
-	// 	if err := block.ValidateBlock(db.latestBlock, db.HashState(), evHandler); err != nil {
-	// 		return nil, err
-	// 	}
+		// Validate the block values and cryptographic audit trail.
+		if err := block.ValidateBlock(db.latestBlock, db.HashState(), evHandler); err != nil {
+			return nil, err
+		}
 
-	// 	// Update the database with the transaction information.
-	// 	for _, tx := range block.MerkleTree.Values() {
-	// 		db.ApplyTransaction(block, tx)
-	// 	}
-	// 	db.ApplyMiningReward(block)
+		// Update the database with the transaction information.
+		for _, tx := range block.MerkleTree.Values() {
+			db.ApplyTransaction(block, tx)
+		}
+		db.ApplyMiningReward(block)
 
-	// 	// Update the current latest block.
-	// 	db.latestBlock = block
-	// }
+		// Update the current latest block.
+		db.latestBlock = block
+	}
 
 	return &db, nil
 }
@@ -82,4 +86,214 @@ func New(genesis genesis.Genesis, storage Storage, evHandler func(v string, args
 // Close closes the open blocks database.
 func (db *Database) Close() {
 	db.storage.Close()
+}
+
+// Query retrieves an account from the database.
+func (db *Database) Query(accountID AccountID) (Account, error) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	acount, exists := db.accounts[accountID]
+	if !exists {
+		return Account{}, errors.New("account does not exist")
+	}
+
+	return acount, nil
+}
+
+// LatestBlock returns the latest block.
+func (db *Database) LatestBlock() Block {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	return db.latestBlock
+}
+
+// HashState returns a hash based on the contents of the accounts and
+// their balances. This is added to each block and checked by peers.
+func (db *Database) HashState() string {
+	accounts := make([]Account, 0, len(db.accounts))
+	db.mu.RLock()
+	{
+		for _, account := range db.accounts {
+			accounts = append(accounts, account)
+		}
+	}
+	db.mu.RUnlock()
+
+	sort.Sort(byAccount(accounts))
+	return signature.Hash(accounts)
+}
+
+// Write adds a new block to the chain.
+func (db *Database) Write(block Block) error {
+	return db.storage.Write(NewBlockData(block))
+}
+
+// UpdateLatestBlock provides safe access to update the latest block.
+func (db *Database) UpdateLatestBlock(block Block) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.latestBlock = block
+}
+
+// ApplyMiningReward gives the specififed account the mining reward.
+func (db *Database) ApplyMiningReward(block Block) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	account := db.accounts[block.Header.BeneficiaryID]
+	account.Balance += block.Header.MiningReward
+
+	db.accounts[block.Header.BeneficiaryID] = account
+}
+
+// ApplyTransaction performs the business logic for applying a transaction
+// to the database.
+func (db *Database) ApplyTransaction(block Block, tx BlockTx) error {
+
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	{
+		// Capture these accounts from the database.
+		from, exists := db.accounts[tx.FromID]
+		if !exists {
+			from = newAccount(tx.FromID, 0)
+		}
+
+		to, exists := db.accounts[tx.ToID]
+		if !exists {
+			to = newAccount(tx.ToID, 0)
+		}
+
+		bnfc, exists := db.accounts[block.Header.BeneficiaryID]
+		if !exists {
+			bnfc = newAccount(block.Header.BeneficiaryID, 0)
+		}
+
+		// The account needs to pay the gas fee regardless. Take the
+		// remaining balance if the account doesn't hold enough for the
+		// full amount of gas. This is the only way to stop bad actors.
+		gasFee := tx.GasPrice * tx.GasUnits
+		if gasFee > from.Balance {
+			gasFee = from.Balance
+		}
+		from.Balance -= gasFee
+		bnfc.Balance += gasFee
+
+		// Make sure these changes get applied.
+		db.accounts[tx.FromID] = from
+		db.accounts[block.Header.BeneficiaryID] = bnfc
+
+		// Perform basic accounting checks.
+		{
+			if tx.Nonce != (from.Nonce + 1) {
+				return fmt.Errorf("transaction invalid, wrong nonce, got %d, exp %d", tx.Nonce, from.Nonce+1)
+			}
+
+			if from.Balance == 0 || from.Balance < (tx.Value+tx.Tip) {
+				return fmt.Errorf("transaction invalid, insufficient funds, bal %d, needed %d", from.Balance, (tx.Value + tx.Tip))
+			}
+		}
+
+		// Update the balances between the two parties.
+		from.Balance -= tx.Value
+		to.Balance += tx.Value
+
+		// Give the beneficiary the tip.
+		from.Balance -= tx.Tip
+		bnfc.Balance += tx.Tip
+
+		// Update the nonce for the next transaction check.
+		from.Nonce = tx.Nonce
+
+		// Update the final changes to these accounts.
+		db.accounts[tx.FromID] = from
+		db.accounts[tx.ToID] = to
+		db.accounts[block.Header.BeneficiaryID] = bnfc
+
+	}
+	return nil
+}
+
+// Remove deletes an account from the database.
+func (db *Database) Remove(accountID AccountID) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	delete(db.accounts, accountID)
+}
+
+// Copy makes a copy of the current accounts in the database.
+func (db *Database) Copy() map[AccountID]Account {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	accounts := make(map[AccountID]Account)
+	for accountID, account := range db.accounts {
+		accounts[accountID] = account
+	}
+	return accounts
+}
+
+func (db *Database) Reset() error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.storage.Reset()
+
+	// Initializes the database back to the genesis information.
+	db.latestBlock = Block{}
+	db.accounts = make(map[AccountID]Account)
+	for accountStr, balance := range db.genesis.Balances {
+		accountID, err := ToAccountID(accountStr)
+		if err != nil {
+			return err
+		}
+
+		db.accounts[accountID] = newAccount(accountID, balance)
+	}
+
+	return nil
+}
+
+// ForEach returns an iterator to walk through all the blocks
+// starting with block number 1.
+func (db *Database) ForEach() DatabaseIterator {
+	return DatabaseIterator{iterator: db.storage.ForEach()}
+}
+
+// GetBlock searches the blockchain on disk to locate and return the
+// contents of the specified block by number.
+func (db *Database) GetBlock(num uint64) (Block, error) {
+	blockData, err := db.storage.GetBlock(num)
+	if err != nil {
+		return Block{}, err
+	}
+
+	return ToBlock(blockData)
+}
+
+// =============================================================================
+
+// DatabaseIterator provides support for iterating over the blocks in the
+// blockchain database using the configured storage option.
+type DatabaseIterator struct {
+	iterator Iterator
+}
+
+// Next retrieves the next block from disk.
+func (di *DatabaseIterator) Next() (Block, error) {
+	blockData, err := di.iterator.Next()
+	if err != nil {
+		return Block{}, err
+	}
+
+	return ToBlock(blockData)
+}
+
+// Done returns the end of chain value.
+func (di *DatabaseIterator) Done() bool {
+	return di.iterator.Done()
 }
